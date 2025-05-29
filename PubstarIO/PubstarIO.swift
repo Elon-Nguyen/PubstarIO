477b84d070008f89631f47a15242d18588f4871d
Elon Nguyen <elon.nguyen@tqcsolution.com>
2025-05-29 15:19:42 +0700

    Initial Commit

---
+++ PubstarIO/PubstarIO.swift
@@ -0,0 +1,9 @@
+//
+//  PubstarIO.swift
+//  PubstarIO
+//
+//  Created by Mobile  on 29/5/25.
+//
+
+import Foundation
+
---
+++ PubstarIOTests/PubstarIOTests.swift
@@ -0,0 +1,17 @@
+//
+//  PubstarIOTests.swift
+//  PubstarIOTests
+//
+//  Created by Mobile  on 29/5/25.
+//
+
+import Testing
+@testable import PubstarIO
+
+struct PubstarIOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
