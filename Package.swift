44860fb9222ae548620624786ee00fd531c7263b
Elon-Nguyen <elon.nguyen@tqcsolution.com>
2025-05-29 16:07:03 +0700

    Create Package.swift
    init Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "PubstarIO",
+    platforms: [
+        .iOS(.v18)
+    ],
+    products: [
+        .library(
+            name: "PubstarIO",
+            targets: ["PubstarIO"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "PubstarIO",
+            url: "https://github.com/Elon-Nguyen/PubstarIO/releases/download/1.0.0/PubstarIO.xcframework.zip",
+            checksum: "706137ae6657c21eb6f7cad0375c09cc8fcb1af5884cb95155e1c7bd6b921ca0" // Run `swift package compute-checksum Pubstar.xcframework.zip` to get `checksum` value.
+        ),
+    ]
+)
